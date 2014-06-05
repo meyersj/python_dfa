321c68a6360f64af134bad6df9f51e2ac10257db
Ruben Niculcea <RubenSandwich@users.noreply.github.com>
2014-06-05 09:58:38 -0700

    Consolidate classes into dfa.py

--- dfa.py
+++ dfa.py
@@ -1,3 +1,5 @@
+import json
+
 DESC = "description"
 STATE = "states"
 START = "start"
@@ -6,6 +8,7 @@ TRANS = "transitions"
 ACCEPT = "accept"
 INPUTS = "inputs"
 
+
 class dfa:
     states = []
     description = ""
@@ -13,37 +16,48 @@ class dfa:
     accept_states = []
     # {State : {input : newState}}
     transitions = {"": {"" : ""}}
+    input_strings = []
     
-    def dfa(self, file_name):
-        parse = parse(file_name)
-        dfa = parse.get_dfa()
-        self.states = dfa[STATES]
+    def __init__(self, file_name):
+        dfa = json.load(open(file_name))
+        self.states = dfa[STATE]
+        self.description = dfa[DESC]
         self.start_state = dfa[START]
         self.accept_states = dfa[ACCEPT]
         self.transitions = dfa[TRANS]
-        
-        for input_str in dfa[INPUTS]
-            run_dfa(input_str)
+        self.input_strings = dfa[INPUTS]
 
-    def run_dfa(input_str):
+    def get_dfa(self):
+    	return self, self.input_strings
+
+    def run_dfa(self, input_str):
         current_state = self.start_state
-        print "Running the DFA on the input: " + str(input_str)
+        print "Running the DFA with the description:"
+        print str(self.description)
+        print "On the input: " + str(input_str)
 
         for char in input_str:
-            if current_state not in transitions:
+            if current_state not in self.transitions:
                 print str(current_state) + " is not a state in the DFA."
                 return
 
-            if char not in transitions[current_state]:
+            if char not in self.transitions[current_state]:
                 print str(char) + " does not transition to a state in the DFA."
                 return
 
-            new_state = transitions[current_state][char]
+            new_state = self.transitions[current_state][char]
             print "Moving from state: " + str(current_state) + " to State: " + str(new_state) + " on input: " + str(char)
             current_state = new_state
 
-        if any(current_state in s for s in accept_states):
-            print "The DFA aceepts the input: " + str(input_str)
+        if any(current_state in s for s in self.accept_states):
+            print "The DFA ACCEPTS the input: " + str(input_str) + "\n"
         else:
-            print "The DFA does not aceept the input: " + str(input_str)
+            print "The DFA REJECTS the input: " + str(input_str) + "\n"
+
+
+if __name__ == "__main__":
+    dfa, input_strings = dfa("machine1.json").get_dfa()
+
+    for s in input_strings:
+    	dfa.run_dfa(s)
 
